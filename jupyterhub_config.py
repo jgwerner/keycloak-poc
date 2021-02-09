2ee777e1e29041bd5686c1bd8327b789079ef85d
Greg Werner <werner.greg@gmail.com>
2021-02-09 12:58:42 -0500

    simplify configs with standard env vars
    Signed-off-by: Greg Werner <werner.greg@gmail.com>

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -25,12 +25,20 @@ c.JupyterHub.tornado_settings = {
 c.JupyterHub.allow_root = True
 c.JupyterHub.allow_origin = '*'
 
+# JupyterHub Postgres connection URI
+postgres_user = os.environ.get('POSTGRES_USER') or 'jupyterhub'
+postgres_password = os.environ.get('POSTGRES_PASSWORD') or 'jupyterhub'
+postgres_host = os.environ.get('POSTGRES_HOST') or 'postgres'
+postgres_port = os.environ.get('POSTGRES_PORT') or '5432'
+postgres_db = os.environ.get('POSTGRES_DB') or 'jupyterhub'
+c.JupyterHub.db_url = f'postgres://{postgres_user}:{postgres_password}/{postgres_host}:{postgres_port}/{postgres_db}'
+
 # Set the authenticator
 c.JupyterHub.authenticator_class = GenericOAuthenticator
 
 c.Authenticator.admin_users = {'admin'}
 c.Authenticator.auto_login = True
-c.Authenticator.allowed_users = {'greg', 'abhi',}
+c.Authenticator.allowed_users = {'greg', 'abhi', 'greg@example.com'}
 
 # Verify TLS certificates.
 if os.environ.get('OAUTH2_TLS_VERIFY') == 'True':
@@ -39,7 +47,7 @@ else:
     c.OAuthenticator.tls_verify = False
 
 # OAuthenticator settings for OAuth2
-c.OAuthenticator.client_id = os.environ.get('OAUTH_CLIENT_ID')
+c.OAuthenticator.client_id = os.environ.get('OAUTH_CLIENT_ID') or 'illumidesk-hub'
 c.OAuthenticator.client_secret = os.environ.get('OAUTH_CLIENT_SECRET')
 c.OAuthenticator.oauth_callback_url = os.environ.get('OAUTH_CALLBACK_URL') or 'http://localhost:8000/hub/oauth_callback'
 c.OAuthenticator.authorize_url = os.environ.get('OAUTH2_AUTHORIZE_URL') or f'http://localhost:8080/auth/realms/illumidesk-realm/protocol/openid-connect/auth'
@@ -49,13 +57,8 @@ c.OAuthenticator.enable_auth_state = True
 # Login service name
 c.GenericOAuthenticator.login_service = os.environ.get('GENERICAUTH_LOGIN_SERVICE_NAME') or 'Keycloak'
 # TODO: clarify scopes
-# c.KeycloakAuthenticator.scope = ['email', 'read_api', 'read_user', 'openid', 'profile']
-c.GenericOAuthenticator.userdata_url = os.environ.get('GENERICAUTH_USERDATA_URL') or 'http://localhost:8080/auth/realms/illumidesk-realm/protocol/openid-connect/userinfo'
+c.GenericOAuthenticator.scope = ['openid']
+c.GenericOAuthenticator.userdata_url = os.environ.get('OAUTH2_USERDATA_URL') or 'http://localhost:8080/auth/realms/illumidesk-realm/protocol/openid-connect/userinfo'
 c.GenericOAuthenticator.userdata_method = os.environ.get('GENERICAUTH_USERDATA_METHOD') or 'GET'
-userdata_params = {
-    os.environ.get('GENERICAUTH_USERNAME_PARAMS_KEY') or 'state' : os.environ.get('GENERICAUTH_USERNAME_PARAMS_VALUE') or 'state' }
-c.GenericOAuthenticator.userdata_params = userdata_params
-c.GenericOAuthenticator.username_key = os.environ.get('GENERICAUTH_USERNAME_KEY') or 'sub'
-
-# Set Keycloak logout url
-c.GenericOAuthenticator.keycloak_logout_url = os.environ.get('KEYCLOAK_LOGOUT_URL') or 'http://localhost:8080/auth/realms/illumidesk-realm/protocol/openid-connect/logout'
+c.GenericOAuthenticator.userdata_params = { 'state': 'state' }
+c.GenericOAuthenticator.username_key = os.environ.get('OAUTH2_USERNAME_KEY') or 'preferred_username'
