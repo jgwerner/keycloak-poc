ce1d1050e48b29dfce7da79d07e9676731736ee9
Greg Werner <werner.greg@gmail.com>
2021-02-18 19:53:23 -0500

    working version with ngrok
    Signed-off-by: Greg Werner <werner.greg@gmail.com>

--- README.md
+++ README.md
@@ -23,7 +23,10 @@ docker-compose up -d
 
 ### 1. Create and Configure Keycloak Realm
 
-1. Log into admin portal at [http://localhost:8080/auth]([http://localhost:8080/auth)
+> **NOTE**: a working local test should use TSL termination. You can emulate TSL termination by using a third party service such as [`ngrok`](https://ngrok.com/download) to publish local ports to a publicly accesible domain with `https://...`.
+
+
+1. Log into admin portal at `https://<my-random-id.ngrok.io>/keycloak/auth`.
 2. Create new realm by navigating to `Home --> Realm Drop Down (top left) --> Create New Realm`.
 3. Enter Realm Name, such as `illumidesk`.
 4. Click on `Configure` --> `Realm Settings`.
@@ -45,7 +48,7 @@ docker-compose up -d
 6. Ensure the `Access Type` option is set to `credentials` (`public` is default).
 7. Ensure `Standard Flow Enabled` is toggled to `ON`.
 8. Ensure `Direct Access Grants Enabled` is toggled to `ON`.
-9. For `Root URL` enter `http://localhost:8000`.
+9. For `Root URL` enter `https://<my-random-id.ngrok.io>`.
 10. For `Base URL` enter `/`.
 11. For `Web Origins` enter `*` (any origin).
 
@@ -60,10 +63,10 @@ Instructions to set up a SAML v2.0 Identity Provider (IdP) vary depending on the
 3. Select the `Regular Web Application` option
 4. Click on the `Create` button
 5. In the `Application URIs` section, ensure the `Token Endpoint Authentication Method` option has `Post` selected.
-6. In the `Application URSs` section, enter the `Allowed Callback URLs` value. This value should have the following format (the example below assumes the host is `http://localhost:8000` and the realm is `illumidesk`)
+6. In the `Application URSs` section, enter the `Allowed Callback URLs` value. This value should have the following format (the example below assumes the host is `https://<my-random-id.ngrok.io>` and the realm is `illumidesk`)
 
 ```
-http://localhost:8080/auth/realms/illumidesk-realm/broker/saml/endpoint
+http://127.0.0.1:8080/keycloak/auth/realms/illumidesk-realm/broker/saml/endpoint
 ```
 
 7. At the bottom of the page, click on the `Advanced Settings` option.
@@ -73,7 +76,7 @@ http://localhost:8080/auth/realms/illumidesk-realm/broker/saml/endpoint
 11. Click on the `Addons` tab.
     1.  Turn on the `SAML2 Web App` by toggling the button to on (green).
     2.  Click on the `SAML2 Web App` card to open the `Settings` and `Usage` modal.
-    3.  Click on the `Settings` tab and enter the `Application Callback URL` for your application. For example, if your host is `http://localhost:8000` and your Realm is `illumidesk`, then your `Application Callback URL` should be `http://localhost:8080/auth/realms/illumidesk/broker/saml/endpoint`.
+    3.  Click on the `Settings` tab and enter the `Application Callback URL` for your application. For example, if your host is `https://<my-random-id.ngrok.io>` and your Realm is `illumidesk`, then your `Application Callback URL` should be `https://<my-random-id.ngrok.io>/keycloak/auth/realms/illumidesk/broker/saml/endpoint`.
 12. Click on the `Connections` tab.
     1.  Enable the `Username-Password-Authentication` by toggling the button so that it's green.
     2.  (Optional) Enable other connections, such as other Social Authentication services.
@@ -88,7 +91,7 @@ http://localhost:8080/auth/realms/illumidesk-realm/broker/saml/endpoint
 4. (Optional) Enter a `Display Name`, such as `IllumiDesk SAML v2.0 Identity Provider (IdP)`
 5. Ensure the `Enabled` option is toggled to `ON`.
 6. Ensure the `Trust Email` option is toggled to `ON`.
-7. The `Service Provider Entity ID` is populated by default. The value should append the `/auth/realms/illumidesk` to the root URL. For example, `http://localhost:8000/auth/realms/illumidesk`.
+7. The `Service Provider Entity ID` is populated by default. The value should append the `/keycloak/auth/realms/illumidesk` to the root URL. For example, `https://<my-random-id.ngrok.io>/keycloak/auth/realms/illumidesk`.
 8. In the `SAML Config` section, add the `Service Provider Entity ID` to reflect the Keycloak realm you set up in **section 1** above.
 9. In the `SAML Config` section, add the `Single Sign-On Service URL`. This value should match the value for the SAML IdP protocol URL. For example, with `Auth0` this setting is called `SAML Protocol URL` in `Applications` --> `<SAML Application Name>` --> `Settings` --> `Advanced Settings` --> `Endpoints` --> `SAML`. The value should be similar to `https://auth.illumidesk.com/samlp/C2Nb4pMdbeAmwLy3dPhr9uB5KMep34ct`.
 10. Select `Unspecified` for `NameID Policy Format`.
@@ -101,9 +104,9 @@ http://localhost:8080/auth/realms/illumidesk-realm/broker/saml/endpoint
 
 1. Copy the `env.example` file and create a `.env` file.
 2. Update the `.env` file with your values:
-   1. `JUPYTERHUB_HOST`: the external facing JupyterHub host URL, such as `http://localhost:8000`.
-   2. `KEYCLOAK_INTERNAL_HOST`: the internal Keycloak service scheme, name and port. For example: `http://keycloak:8080`.
-   3. `KEYCLOAK_EXTERNAL_HOST`: the external Keycloak scheme, host, and port. This is the endpoint that For example:  `http://localhost:8080`.
+   1. `JUPYTERHUB_HOST`: the external facing JupyterHub host URL, such as `https://<my-random-id.ngrok.io>`.
+   2. `KEYCLOAK_INTERNAL_HOST`: the internal Keycloak service scheme, name and port. For example: `https://keycloak:8080`.
+   3. `KEYCLOAK_EXTERNAL_HOST`: the external Keycloak scheme, host, and port. This is the endpoint that For example:  `https://127.0.0.1:8080`.
    4. `KEYCLOAK_REALM`: the Keycloak Realm name, such as `illumidesk`.
    5. `OAUTH_CLIENT_ID`: the name of the Keycloak client configured with the Keycloak Realm in **Section 4** above, such as `illumidesk-hub`.
    6. `JUPYTERHUB_CRYPT_KEY`: the JupyterHub crytographic key used to encrypt the `auth_state` when the authentication dictionary is persisted from the Authenticator to the Spawner using the `JupyterHub.auth_state_enabled = True` setting. Create a secure random string with the `openssl rand -hex 32` command from your preferred terminal. If you don't have access to the `openssl` command, any random value should suffice. **However, please change before configuring the environment for Production!**
--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -1,136 +1,46 @@
-import json, os, urllib
+""" Configuration file for jupyterhub. """
+import os
 
 from dotenv import load_dotenv
 
-from tornado.auth import OAuth2Mixin
-from tornado import gen, web
-from tornado.httputil import url_concat
-from tornado.httpclient import HTTPRequest, AsyncHTTPClient
-
-from jupyterhub.auth import LocalAuthenticator
-from jupyterhub.handlers import LogoutHandler
-from jupyterhub.utils import url_path_join
-
-from oauthenticator.oauth2 import OAuthLoginHandler, OAuthenticator
+from oauthenticator.generic import GenericOAuthenticator
 
 
 load_dotenv()
 
 
-oidc_client_id = os.getenv('OIDC_CLIENT_ID')    
-oidc_server_host = os.getenv('OIDC_SERVER')
+c.JupyterHub.allow_root = True
+c.JupyterHub.allow_origin = '*'
 
+# Set port and IP
+c.JupyterHub.ip = '0.0.0.0'
+c.JupyterHub.port = 8000
 
-class KeycloakMixin(OAuth2Mixin):
+# Set log level
+c.Application.log_level = 'DEBUG'
 
-    _OAUTH_AUTHORIZE_URL = "{}/auth/realms/{}/protocol/openid-connect/auth".format(oidc_server_host, oidc_client_id)
-    _OAUTH_ACCESS_TOKEN_URL = "{}/auth/realms/{}/protocol/openid-connect/token".format(oidc_server_host, oidc_client_id)
-    _OAUTH_LOGOUT_URL = "{}/auth/realms/{}/protocol/openid-connect/logout".format(oidc_server_host, oidc_client_id)
-    _OAUTH_USERINFO_URL = "{}/auth/realms/{}/protocol/openid-connect/userinfo".format(oidc_server_host, oidc_client_id)
+# Header settings for iFrame and SameSite
+c.JupyterHub.tornado_settings = {
+    "headers": {"Content-Security-Policy": "frame-ancestors 'self' *"},
+    "cookie_options": {"SameSite": "None", "Secure": True},
+}
 
-class KeycloakLoginHandler(OAuthLoginHandler, KeycloakMixin):
-    pass
+# Upgrade the database automatically on start
+c.JupyterHub.upgrade_db = True
 
-class KeycloakLogoutHandler(LogoutHandler, KeycloakMixin):
-    def get(self):
-        params = dict(
-            redirect_uri="%s://%s%slogout" % (
-                self.request.protocol, self.request.host,
-                self.hub.server.base_url)
-        )
-        logout_url = KeycloakMixin._OAUTH_LOGOUT_URL
-        logout_url = url_concat(logout_url, params)
-        self.redirect(logout_url, permanent=False)
+# JupyterHub Postgres connection URI
+postgres_user = os.environ.get('POSTGRES_USER') or 'jupyterhub'
+postgres_password = os.environ.get('POSTGRES_PASSWORD') or 'jupyterhub'
+postgres_host = os.environ.get('POSTGRES_HOST') or 'postgres'
+postgres_port = os.environ.get('POSTGRES_PORT') or '5432'
+postgres_db = os.environ.get('POSTGRES_DB') or 'jupyterhub'
+c.JupyterHub.db_url = f'postgres://{postgres_user}:{postgres_password}/{postgres_host}:{postgres_port}/{postgres_db}'
 
-class KeycloakOAuthenticator(OAuthenticator, KeycloakMixin):
-    login_service = "OIDC"
-    login_handler = KeycloakLoginHandler
-
-    authorize_url = "{}/auth/realms/{}/protocol/openid-connect/auth".format(oidc_server_host, oidc_client_id)
-    access_token_url = "{}/auth/realms/{}/protocol/openid-connect/token".format(oidc_server_host, oidc_client_id)
-    oauth_logout_url = "{}/auth/realms/{}/protocol/openid-connect/logout".format(oidc_server_host, oidc_client_id)
-    oauth_userinfo_url = "{}/auth/realms/{}/protocol/openid-connect/userinfo".format(oidc_server_host, oidc_client_id)
-
-    def check_whitelist(self, username, authentication=None):
-        self.log.info('Checking whilelist for username: %r', username)
-        return True
-
-    def logout_url(self, base_url):
-        return url_path_join(base_url, 'oauth_logout')
-
-    def get_handlers(self, app):
-        handlers = OAuthenticator.get_handlers(self, app)
-        handlers.extend([(r'/oauth_logout', KeycloakLogoutHandler)])
-        return handlers
-
-    @gen.coroutine
-    def authenticate(self, handler, data=None):
-        code = handler.get_argument("code", False)
-        self.log.info('code: %s', code)
-        if not code:
-            raise web.HTTPError(400, "oauth callback made without a token")
-        http_client = AsyncHTTPClient()
-        params = dict(
-            grant_type='authorization_code',
-            code=code,
-            redirect_uri=self.get_callback_url(handler),
-        )
-        token_url = KeycloakMixin._OAUTH_ACCESS_TOKEN_URL
-        token_req = HTTPRequest(
-            token_url,
-            method="POST",
-            headers={
-                "Accept": "application/json",
-                 "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
-            },
-            auth_username = self.client_id,
-            auth_password = self.client_secret,
-            body = urllib.parse.urlencode(params).encode('utf-8'),
-            )
-        token_resp = yield http_client.fetch(token_req)
-        token_resp_json = json.loads(token_resp.body.decode('utf8', 'replace'))
-        access_token = token_resp_json['access_token']
-        if not access_token:
-            raise web.HTTPError(400, "failed to get access token")
-        self.log.info('oauth token: %r', access_token)
-        user_info_url = KeycloakMixin._OAUTH_USERINFO_URL
-        user_info_req = HTTPRequest(
-            user_info_url,
-            method="GET",
-            headers={
-                "Accept": "application/json",
-                "Authorization": "Bearer %s" % access_token
-                },
-            )
-        user_info_res = yield http_client.fetch(user_info_req)
-        self.log.info('user_info_res: %r', user_info_res)
-        user_info_res_json = json.loads(user_info_res.body.decode('utf8', 'replace'))
-        self.log.info('user_info_res_json: %r', user_info_res_json)
-        return {
-            'name': user_info_res_json['preferred_username'],
-            'auth_state': {
-                'upstream_token': user_info_res_json,
-            },
-        }
-
-# --- Proxy ---
-c.ConfigurableHTTPProxy.api_url = 'http://jupyterhub:8081'
-
-# --- Common ---
-c.JupyterHub.base_url = '/jupyter'
-c.JupyterHub.port = 8080
-c.JupyterHub.cookie_secret = bytes.fromhex('c336ff8bc0f477928cfc73454821ee11182e90a49de57f81e0919e66851349c6')
-c.ConfigurableHTTPProxy.auth_token = '0bc02bede919e99a26de1e2a7a5aadfaf6228de836ec39a05a6c6942831d8fe5'
-c.JupyterHub.confirm_no_ssl = True
-c.Authenticator.enable_auth_state = True
-
-# --- Authenticator ---
 # Set the authenticator
-c.JupyterHub.authenticator_class = KeycloakOAuthenticator
+c.JupyterHub.authenticator_class = GenericOAuthenticator
 
-# Accept admins and users
 c.Authenticator.admin_users = {'admin'}
-c.Authenticator.auto_login = True
+c.Authenticator.auto_login = False
 c.Authenticator.allowed_users = {'greg', 'abhi', 'greg@example.com'}
 
 # Verify TLS certificates.
@@ -139,29 +49,19 @@ if os.environ.get('OAUTH2_TLS_VERIFY') == 'True':
 else:
     c.OAuthenticator.tls_verify = False
 
-c.KeycloakOAuthenticator.client_id = oidc_client_id
-c.KeycloakOAuthenticator.client_secret = os.getenv('OIDC_SECRET')
-c.Authenticator.auto_login = True
+# OAuthenticator settings for OAuth2
+c.OAuthenticator.client_id = os.environ.get('OAUTH_CLIENT_ID') or 'illumidesk-hub'
+c.OAuthenticator.client_secret = os.environ.get('OAUTH_CLIENT_SECRET')
+c.OAuthenticator.oauth_callback_url = os.environ.get('OAUTH_CALLBACK_URL') or 'http://127.0.0.1/hub/oauth_callback'
+c.OAuthenticator.authorize_url = os.environ.get('OAUTH2_AUTHORIZE_URL') or f'http://127.0.0.1/auth/realms/illumidesk-realm/protocol/openid-connect/auth'
+c.OAuthenticator.token_url = os.environ.get('OAUTH2_TOKEN_URL') or f'http://127.0.0.1/auth/realms/illumidesk-realm/protocol/openid-connect/token'
+c.OAuthenticator.enable_auth_state = True
 
-# --- Spawner ---
-c.Spawner.debug = True
-c.Spawner.default_url = '/lab'
-c.Spawner.args = ['--NotebookApp.tornado_settings={"headers":{"Content-Security-Policy": "frame-ancestors *"}}']   
-c.JupyterHub.tornado_settings = {
-    "headers": {"Content-Security-Policy": "frame-ancestors 'self' *"},
-    "cookie_options": {"SameSite": "None", "Secure": True},
-}
-c.NotebookApp.tornado_settings = {
-    "headers": {"Content-Security-Policy": "frame-ancestors 'self' *"},
-    "cookie_options": {"SameSite": "None", "Secure": True},
-}
-
-def docker_spawner(c):
-    c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
-    c.DockerSpawner.image = 'jupyterhub/singleuser:1.2'
-    c.DockerSpawner.default_url = '/lab'
-    c.DockerSpawner.remove_containers = True
-    from jupyter_client.localinterfaces import public_ips
-    c.JupyterHub.hub_ip = public_ips()[0]
-
-docker_spawner(c)
+# Login service name
+c.GenericOAuthenticator.login_service = os.environ.get('GENERICAUTH_LOGIN_SERVICE_NAME') or 'Keycloak'
+# TODO: clarify scopes
+c.GenericOAuthenticator.scope = ['openid']
+c.GenericOAuthenticator.userdata_url = os.environ.get('OAUTH2_USERDATA_URL') or 'http://127.0.0.1/auth/realms/illumidesk-realm/protocol/openid-connect/userinfo'
+c.GenericOAuthenticator.userdata_method = os.environ.get('GENERICAUTH_USERDATA_METHOD') or 'GET'
+c.GenericOAuthenticator.userdata_params = { 'state': 'state' }
+c.GenericOAuthenticator.username_key = os.environ.get('OAUTH2_USERNAME_KEY') or 'preferred_username'
